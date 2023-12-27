ec4343e6848da7dfc4d09f0bcda80e365122463c
Sascha Stropek <77939896+sastro2@users.noreply.github.com>
2023-12-27 15:09:14 +0100

    fix: postgres

--- src/methods/dataAccess/db.ts
+++ src/methods/dataAccess/db.ts
@@ -1,6 +1,6 @@
 import postgres from 'postgres';
 
-const databaseInstance = postgres(`${process.env.DATABASE_URL}`);
+const databaseInstance = postgres(`${process.env.DATABASE_URL}`, {ssl: {require: true, rejectUnauthorized: false}});
 
 
 export default databaseInstance;
