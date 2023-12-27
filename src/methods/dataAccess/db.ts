a2d63ed1c7e5d2edb7800d8d8cbc20715da7a34c
Sascha Stropek <77939896+sastro2@users.noreply.github.com>
2023-12-27 14:14:34 +0100

    fix: database reject unauthorized set to true

--- src/methods/dataAccess/db.ts
+++ src/methods/dataAccess/db.ts
@@ -1,6 +1,6 @@
 import postgres from 'postgres';
 
-const databaseInstance = postgres(`${process.env.DATABASE_URL}`, {ssl: {require: true}});
+const databaseInstance = postgres(`${process.env.DATABASE_URL}`, {ssl: {require: true, rejectUnauthorized: true}});
 
 
 export default databaseInstance;
